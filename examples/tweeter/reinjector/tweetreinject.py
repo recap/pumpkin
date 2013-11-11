403198ab5437d79c03abe4ddc86ae57efe9c284c
Reggie <cushingreggie@gmail.com>
2013-11-11 14:24:26 +0100

    reinject

--- examples/tweeter/reinjector/tweetreinject.py
+++ examples/tweeter/reinjector/tweetreinject.py
@@ -37,13 +37,13 @@ from random import randint
 
 
 ###
-import nltk
-from nltk.corpus import cmudict
-import curses
-from curses.ascii import isdigit
-import nltk
-from nltk.corpus import cmudict
-import re
+#import nltk
+#from nltk.corpus import cmudict
+#import curses
+#from curses.ascii import isdigit
+#import nltk
+#from nltk.corpus import cmudict
+#import re
 ###
 
 import zmq
@@ -97,87 +97,87 @@ class tweetreinject(PmkSeed.Seed):
         #log.debug("Sending...")
         self.sock.send(tweet)
 
-    def _pre(self):
-        nltk.data.path.append(os.getcwd()+"/examples/tweeter/nltk_data")
-        pass
-    def is_english(self, text):
-
-        #nltk.download(nltk.corpus.stopwords)
-
-        ENGLISH_STOPWORDS = set(nltk.corpus.stopwords.words('english'))
-        NON_ENGLISH_STOPWORDS = set(nltk.corpus.stopwords.words()) - ENGLISH_STOPWORDS
-
-        text = text.lower()
-        words = set(nltk.wordpunct_tokenize(text))
-        return len(words & ENGLISH_STOPWORDS) > len(words & NON_ENGLISH_STOPWORDS)
-
-    def is_a(self,text):
-        m = re.search('([A-Z]+[A-Za-z]+\s*[A-Za-z]*\s(has an|has a)\s[A-Z]+[A-Za-z]+)', text, re.S)
-        if m:
-            tw = m.group(0)
-            return tw
-
-    def __testfilter(self, tweet):
-
-
-        m = re.search('W(\s+)(.*)(\n)', tweet, re.S)
-        if m:
-            tw = m.group(2)
-            self.is_a(tw)
-
-        pass
-
-    def is_haiku(self, text):
-
-        text_orig = text
-        text = text.lower()
-        if filter(str.isdigit, str(text)):
-            return False
-        words = nltk.wordpunct_tokenize(re.sub('[^a-zA-Z_ ]', '',text))
-        #print words
-        syl_count = 0
-        word_count = 0
-        haiku_line_count = 0
-        lines = []
-        d = cmudict.dict()
-
-        for word in words:
-            if word.lower() in d.keys():
-                syl_count += [len(list(y for y in x if isdigit(y[-1]))) for x in
-                        d[word.lower()]][0]
-            if haiku_line_count == 0:
-                if syl_count == 5:
-                    lines.append(word)
-                    haiku_line_count += 1
-            elif haiku_line_count == 1:
-                if syl_count == 12:
-                    lines.append(word)
-                    haiku_line_count += 1
-            else:
-                if syl_count == 17:
-                    lines.append(word)
-                    haiku_line_count += 1
-
-        if syl_count == 17:
-            try:
-                final_lines = []
-
-                str_tmp = ""
-                counter = 0
-                for word in text_orig.split():
-                    str_tmp += str(word) + " "
-                    if lines[counter].lower() in str(word).lower():
-                        final_lines.append(str_tmp.strip())
-                        counter += 1
-                        str_tmp = ""
-                if len(str_tmp) > 0:
-                    final_lines.append(str_tmp.strip())
-                return True
-
-            except Exception as e:
-                print e
-                return False
-        else:
-            return False
-
-        return True
+    #def _pre(self):
+    #    nltk.data.path.append(os.getcwd()+"/examples/tweeter/nltk_data")
+    #    pass
+    #def is_english(self, text):
+    #
+    #    #nltk.download(nltk.corpus.stopwords)
+    #
+    #    ENGLISH_STOPWORDS = set(nltk.corpus.stopwords.words('english'))
+    #    NON_ENGLISH_STOPWORDS = set(nltk.corpus.stopwords.words()) - ENGLISH_STOPWORDS
+    #
+    #    text = text.lower()
+    #    words = set(nltk.wordpunct_tokenize(text))
+    #    return len(words & ENGLISH_STOPWORDS) > len(words & NON_ENGLISH_STOPWORDS)
+    #
+    #def is_a(self,text):
+    #    m = re.search('([A-Z]+[A-Za-z]+\s*[A-Za-z]*\s(has an|has a)\s[A-Z]+[A-Za-z]+)', text, re.S)
+    #    if m:
+    #        tw = m.group(0)
+    #        return tw
+    #
+    #def __testfilter(self, tweet):
+    #
+    #
+    #    m = re.search('W(\s+)(.*)(\n)', tweet, re.S)
+    #    if m:
+    #        tw = m.group(2)
+    #        self.is_a(tw)
+    #
+    #    pass
+    #
+    #def is_haiku(self, text):
+    #
+    #    text_orig = text
+    #    text = text.lower()
+    #    if filter(str.isdigit, str(text)):
+    #        return False
+    #    words = nltk.wordpunct_tokenize(re.sub('[^a-zA-Z_ ]', '',text))
+    #    #print words
+    #    syl_count = 0
+    #    word_count = 0
+    #    haiku_line_count = 0
+    #    lines = []
+    #    d = cmudict.dict()
+    #
+    #    for word in words:
+    #        if word.lower() in d.keys():
+    #            syl_count += [len(list(y for y in x if isdigit(y[-1]))) for x in
+    #                    d[word.lower()]][0]
+    #        if haiku_line_count == 0:
+    #            if syl_count == 5:
+    #                lines.append(word)
+    #                haiku_line_count += 1
+    #        elif haiku_line_count == 1:
+    #            if syl_count == 12:
+    #                lines.append(word)
+    #                haiku_line_count += 1
+    #        else:
+    #            if syl_count == 17:
+    #                lines.append(word)
+    #                haiku_line_count += 1
+    #
+    #    if syl_count == 17:
+    #        try:
+    #            final_lines = []
+    #
+    #            str_tmp = ""
+    #            counter = 0
+    #            for word in text_orig.split():
+    #                str_tmp += str(word) + " "
+    #                if lines[counter].lower() in str(word).lower():
+    #                    final_lines.append(str_tmp.strip())
+    #                    counter += 1
+    #                    str_tmp = ""
+    #            if len(str_tmp) > 0:
+    #                final_lines.append(str_tmp.strip())
+    #            return True
+    #
+    #        except Exception as e:
+    #            print e
+    #            return False
+    #    else:
+    #        return False
+    #
+    #    return True
