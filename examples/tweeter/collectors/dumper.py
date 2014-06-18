52e139937e890452efab1eb6e30bfec1df559e66
Reggie <cushingreggie@gmail.com>
2014-06-18 12:25:41 +0200

    dumper

--- examples/tweeter/collectors/dumper.py
+++ examples/tweeter/collectors/dumper.py
@@ -22,19 +22,13 @@ __author__ = 'reggie'
 
 
 
-import re
-
 
 from pumpkin import PmkSeed
 
-import json
-import re
-import networkx as nx
-from networkx.readwrite import json_graph
 
 
 
-class collectorhaiku(PmkSeed.Seed):
+class dumper(PmkSeed.Seed):
 
     def __init__(self, context, poi=None):
         PmkSeed.Seed.__init__(self, context,poi)
