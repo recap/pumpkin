a6e5cc2c21053b8b4e44612aa929623eeec9c767
Reggie <cushingreggie@gmail.com>
2014-06-23 14:14:31 +0200

    sp_tweeterinjector

--- examples/tweeter/soa/sp_tweetinject.py
+++ examples/tweeter/soa/sp_tweetinject.py
@@ -1,55 +1,46 @@
-__author__ = 'reggie'
-
-
 ###START-CONF
 ##{
-##"object_name": "filterenglish",
+##"object_name": "sp_tweetinject",
 ##"object_poi": "qpwo-2345",
+##"auto-load": true,
+##"remoting" : false,
 ##"parameters": [
-##                 {
-##                      "name": "tweet",
-##                      "description": "raw numbers",
-##                      "required": true,
-##                      "type": "TweetString",
-##                      "format": "",
-##                      "state" : "RAW"
-##                  }
+##
 ##              ],
 ##"return": [
 ##              {
 ##                      "name": "tweet",
-##                      "description": "english or non-english tweet",
+##                      "description": "raw tweet",
 ##                      "required": true,
 ##                      "type": "TweetString",
 ##                      "format": "",
-##                      "state" : "ENGLISH|NONENGLISH"
+##                      "state" : "RAW"
 ##                  }
 ##
 ##          ] }
 ##END-CONF
 
 
-
 import re
 import nltk
 import time
-from pumpkin import PmkSeed
 
+from os import listdir
+from os.path import isfile, join
+import pika
+from os.path import expanduser
 
+from pumpkin import *
 
-class filterenglish(PmkSeed.Seed):
+class sp_tweetinject(PmkSeed.Seed):
 
     def __init__(self, context, poi=None):
         PmkSeed.Seed.__init__(self, context,poi)
-        self.eng_cnt = 0
-        self.neng_cnt = 0
-        self.count = 0
 
-
-        pass
 
     def on_load(self):
         print "Loading: " + self.__class__.__name__
+
         wd = self.context.getWorkingDir()
         nltk.data.path.append(wd + "nltk_data")
 
@@ -59,15 +50,13 @@ class filterenglish(PmkSeed.Seed):
 
         self.tm = time.time()
 
-        pass
-
     def is_english(self, text):
 
         text = text.lower()
         words = set(nltk.wordpunct_tokenize(text))
         return len(words & self.ENGLISH_STOPWORDS) > len(words & self.NON_ENGLISH_STOPWORDS)
 
-    def run(self, pkt, data):
+    def fil_run(self, pkt, data):
         tweet = data
         m = re.search('W(\s+)(.*)(\n)', tweet, re.S)
         if m:
@@ -88,3 +77,28 @@ class filterenglish(PmkSeed.Seed):
                 #self.dispatch(pkt, tweet, "NONENGLISH")
                 self.neng_cnt += 1
         pass
+
+    def run(self, pkt):
+        dir = expanduser("~")+"/tweets/"
+        onlyfiles = [ f for f in listdir(dir) if isfile(join(dir,f)) ]
+        for fl in onlyfiles:
+            fullpath = dir+fl
+            if( fl[-3:] == "txt"):
+                print "File: "+str(fl)
+                with open(fullpath) as f:
+                    for line in f:
+                        if line.startswith('T'):
+                            tweet = line
+                        if line.startswith("U"):
+                            tweet = tweet + line
+                        if line.startswith("W"):
+                            if line == "No Post Title":
+                                line =""
+                            else:
+                                tweet = tweet + line
+                                self.fil_run(pkt, tweet)
+                                #self.dispatch(pkt, tweet, "RAW")
+                                del line
+                                del tweet
+
+
