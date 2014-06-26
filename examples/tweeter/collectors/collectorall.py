45691adbf13d1675a125ef3bf2e85b95e3730b19
Reggie <cushingreggie@gmail.com>
2014-06-26 16:35:57 +0200

    collector all

---
+++ examples/tweeter/collectors/collectorall.py
@@ -0,0 +1,51 @@
+__author__ = 'reggie'
+
+
+###START-CONF
+##{
+##"object_name": "collectorall",
+##"object_poi": "qpwo-2345",
+##"parameters": [
+##                 {
+##                      "name": "tweet",
+##                      "description": "english haiku tweets",
+##                      "required": true,
+##                      "type": "TweetString",
+##                      "format": "",
+##                      "state" : "ISA|HASA|HAIKU|NONENGLISH"
+##                  }
+##              ],
+##"return": [
+##
+##          ] }
+##END-CONF
+
+
+
+import re
+
+
+from pumpkin import PmkSeed
+
+import json
+import re
+import networkx as nx
+from networkx.readwrite import json_graph
+
+
+
+class collectorall(PmkSeed.Seed):
+
+    def __init__(self, context, poi=None):
+        PmkSeed.Seed.__init__(self, context,poi)
+
+        pass
+
+    def on_load(self):
+        print "Loading: " + self.__class__.__name__
+        pass
+
+
+    def run(self, pkt, tweet):
+        pass
+
