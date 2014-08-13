09bd64ac1607e996770f666f50066f2d637e8e1f
Reggie <cushingreggie@gmail.com>
2014-08-13 09:14:15 +0200

    rabbitmq vlan

--- pumpkin/PmkInternalDispatch.py
+++ pumpkin/PmkInternalDispatch.py
@@ -127,7 +127,7 @@ class RabbitMQMonitor():
             #self.channel.queue_declare(queue=str(queue))
             #self.channel.queue_bind(exchange=str(exchange),
             #       queue=str(queue))
-            self.channel.queue_declare(queue=str(queue), durable=False)
+            self.channel.queue_declare(queue=str(queue), durable=False, exclusive=True)
             #self.channel.basic_consume(self.callback,
             #          queue=queue,
             #          no_ack=True)
