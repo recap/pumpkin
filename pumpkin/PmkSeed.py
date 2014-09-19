6ba7a3acb3ded90c22879df9ed824d84cc74b3ac
Reggie <cushingreggie@gmail.com>
2014-09-19 13:44:13 +0200

    stuff

--- pumpkin/PmkSeed.py
+++ pumpkin/PmkSeed.py
@@ -135,7 +135,7 @@ class Seed(object):
         ship_id = self.context.getExecContext()
         cont_id =  str(uuid.uuid4())[:8]
         pkt.append({"ship" : ship_id, "container" : cont_id, "box" : '0', "fragment" : '0', "e" : 0, "state": "NEW", \
-                    "c_tag" : "NONE:NONE", "ttl" : 'D',"t_state":"None", "t_otype" : "None" ,"stop_func": "None",\
+                    "c_tag" : "NONE:NONE", "ttl" : 'D', "aux": 0, "t_state":"None", "t_otype" : "None" ,"stop_func": "None",\
                     "last_contact" : "None", "las"
                                              "t_func" : "None", "last_timestamp" : 0})
         #Place holder for data automaton
