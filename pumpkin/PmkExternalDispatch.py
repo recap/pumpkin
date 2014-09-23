505cfd961b685b7f0e22d7e19e45dc8f61f38671
Reggie <cushingreggie@gmail.com>
2014-09-23 15:37:21 +0200

    ip exception

--- pumpkin/PmkExternalDispatch.py
+++ pumpkin/PmkExternalDispatch.py
@@ -441,13 +441,13 @@ class ZMQPacketDispatch(Dispatch):
 
     def dispatch(self, pkt):
 
-        try:
-            if not self.__check_ep(self.ep):
-                raise Exception("Endpoint closed")
+        #try:
+        #    if not self.__check_ep(self.ep):
+        #        raise Exception("Endpoint closed")
 
-        except Exception as e:
-            logging.error("Failed to connect to: "+self.ep)
-            return
+        #except Exception as e:
+        #    logging.error("Failed to connect to: "+self.ep)
+        #    return
 
 
         logging.debug("Sending message: "+json.dumps(pkt))
