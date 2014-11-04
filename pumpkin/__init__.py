69ed5a2a4fab49145bb4588e92a37daf18615d90
Reggie <cushingreggie@gmail.com>
2014-11-04 15:18:07 +0100

    version

--- pumpkin/__init__.py
+++ pumpkin/__init__.py
@@ -8,7 +8,7 @@ required_version = (2, 3)
 if sys.version_info < required_version:
     raise ImportError, "Requires at least Python 2.3"
 
-VERSION = "0.3.1"
+VERSION = "kakai-beta"
 
 from pumpkin.PmkDataCatch import *
 from pumpkin.PmkBroadcast import *  
