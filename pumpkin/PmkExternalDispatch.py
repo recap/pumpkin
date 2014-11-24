6a021144bdeefa0101f85df5af265d6ec2d4feed
Reggie <cushingreggie@gmail.com>
2014-11-24 19:24:06 +0100

    back pressure

--- pumpkin/PmkExternalDispatch.py
+++ pumpkin/PmkExternalDispatch.py
@@ -547,7 +547,7 @@ class EndpointPicker(object):
                                 bklog = ep["wshift"]
                                 bklog -= et
                                 if bklog > 0:
-                                    print "BACKLOG: "+str(bklog)
+                                    #print "BACKLOG: "+str(bklog)
                                     continue
                                 else:
                                     ep["wshift"] = 0
@@ -570,7 +570,7 @@ class EndpointPicker(object):
                                     #print "Adding: "+str(y)
                                     #adding
                                     ep["wait"] = (y) #+ b
-                                    print "WAIT: "+str(y)+" WSHIFT: "+str(b)+" X: "+str(x)+" M: "+str(m)+" C: "+str(c)
+                                    #print "WAIT: "+str(y)+" WSHIFT: "+str(b)+" X: "+str(x)+" M: "+str(m)+" C: "+str(c)
                                     #print "SETTING: "+str(b)
                                     ep["wshift"] = b
                                     pred[2] = 0
