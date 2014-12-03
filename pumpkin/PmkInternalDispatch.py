6673015e7909271336397efa4b71cc620c38fe47
Reggie <cushingreggie@gmail.com>
2014-12-03 14:23:07 +0100

    zmq

--- pumpkin/PmkInternalDispatch.py
+++ pumpkin/PmkInternalDispatch.py
@@ -436,6 +436,7 @@ class ZMQPacketMonitor(SThread):
                 #if "multiple" not in pkt[0].keys():
                 #dig(pkt)
                 queue_put(pkt)
+                time.sleep(2)
                 soc.send("OK")
                 #self.proccess_pkt(msg)
                 #del msg
