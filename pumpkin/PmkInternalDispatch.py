60f473ee7cd3ff028e0036f9a653cfb67fb3754f
Reggie <cushingreggie@gmail.com>
2014-12-04 17:48:22 +0100

    sync client server

--- pumpkin/PmkInternalDispatch.py
+++ pumpkin/PmkInternalDispatch.py
@@ -470,14 +470,14 @@ class ZMQPacketMonitor(SThread):
                 #print "RECEIVED QUEUE: "+str(rx.qsize())
                 #self.context.getRx().put(msg)
 
-                print "Received: "+str(sys.getsizeof(msg))
-                tm1 = time.time()
-                pkt = json.loads(zlib.decompress(msg))
-                tm2 = time.time()
-
-                t_diff = tm2 - tm1
-
-                print "Decompress time: {0:.12f}".format(t_diff)
+                # print "Received: "+str(sys.getsizeof(msg))
+                # tm1 = time.time()
+                # pkt = json.loads(zlib.decompress(msg))
+                # tm2 = time.time()
+                #
+                # t_diff = tm2 - tm1
+                #
+                # print "Decompress time: {0:.12f}".format(t_diff)
                 #pkt = json.loads(msg)
                 #if "multiple" not in pkt[0].keys():
                 #dig(pkt)
