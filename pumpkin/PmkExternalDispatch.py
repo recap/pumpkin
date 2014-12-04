04904ee0b44961ab48724f7b517020415e3fc8f7
Reggie <cushingreggie@gmail.com>
2014-12-04 15:33:59 +0100

    sync client server

--- pumpkin/PmkExternalDispatch.py
+++ pumpkin/PmkExternalDispatch.py
@@ -287,11 +287,13 @@ class ExternalDispatch(SThread):
                                     #     if eff >= (peff - (peff*0.10)):
                                     if eff > peff:
 
-                                        self._bunch += 20
-                                        # if self._bunch < 2000:
-                                        #     self._bunch = self._bunch * 2 + 1
-                                        # else:
-                                        #     self._bunch += 200
+                                        #self._bunch += 20
+
+
+                                        if eff  < 0.4:
+                                            self._bunch = self._bunch * 2 + 1
+                                        else:
+                                            self._bunch += 200
 
                                         self._pval = (eff, n, peff, pn)
                                         self._set_bunch = True
