09c55c82829308ab5d73242b5ca302d167ded401
Reggie <cushingreggie@gmail.com>
2014-12-04 14:54:04 +0100

    sync client server

--- pumpkin/PmkInternalDispatch.py
+++ pumpkin/PmkInternalDispatch.py
@@ -104,7 +104,7 @@ class InternalDispatch(SThread):
                     st_overhead = "{0:.12f}".format(overhead)
                     st_eff = "{0:.12f}".format(eff)
 
-                    #print "TIME DISPATCHED: "+pkt[0]["timestamp"]+" TIME REC: "+st_tm+" PKT RPTTM: "+st_tdelta+" EXEC TIME: "+st_mexec+" OVERHEAD: "+st_overhead+" EFF: "+st_eff+" BUNCH: "+str(n)
+                    print "TIME DISPATCHED: "+pkt[0]["timestamp"]+" TIME REC: "+st_tm+" PKT RPTTM: "+st_tdelta+" EXEC TIME: "+st_mexec+" OVERHEAD: "+st_overhead+" EFF: "+st_eff+" BUNCH: "+str(n)
                     print st_tm+" "+st_eff+" "+str(n)
 
                     #print json.dumps(pkt)
